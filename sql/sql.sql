9a5fdb9abf7dd03d4e12c9189d7fc6abc02479d3
Sergey <silver2006@tut.by>
2017-07-18 22:00:58 +0300

    Recreate database, editing the types of relationships between tables

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,79 +1,153 @@
-CREATE TABLE `category` (
-  `CategoryID` int(11) NOT NULL AUTO_INCREMENT,
-  `CategoryName` varchar(45) NOT NULL,
-  `CategoryDescription` varchar(200) NOT NULL,
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema eshop
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema eshop
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `eshop` DEFAULT CHARACTER SET utf8 ;
+USE `eshop` ;
+
+-- -----------------------------------------------------
+-- Table `eshop`.`category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eshop`.`category` (
+  `CategoryID` INT(11) NOT NULL AUTO_INCREMENT,
+  `CategoryName` VARCHAR(45) NOT NULL,
+  `CategoryDescription` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`CategoryID`),
-  UNIQUE KEY `idCategory_UNIQUE` (`CategoryID`),
-  UNIQUE KEY `CategoryName_UNIQUE` (`CategoryName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  UNIQUE INDEX `idCategory_UNIQUE` (`CategoryID` ASC),
+  UNIQUE INDEX `CategoryName_UNIQUE` (`CategoryName` ASC))
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `customer` (
-  `CustomerID` int(11) NOT NULL AUTO_INCREMENT,
-  `FirstName` varchar(45) NOT NULL,
-  `LastName` varchar(45) NOT NULL,
-  `Age` date NOT NULL,
-  `Phone` int(11) NOT NULL,
-  `City` varchar(30) NOT NULL,
-  `Address` varchar(45) NOT NULL,
-  `Email` varchar(45) NOT NULL,
-  `Password` varchar(45) NOT NULL,
+
+-- -----------------------------------------------------
+-- Table `eshop`.`customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eshop`.`customer` (
+  `CustomerID` INT(11) NOT NULL AUTO_INCREMENT,
+  `FirstName` VARCHAR(45) NOT NULL,
+  `LastName` VARCHAR(45) NOT NULL,
+  `Age` DATE NOT NULL,
+  `Phone` INT(11) NOT NULL,
+  `City` VARCHAR(30) NOT NULL,
+  `Address` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
+  `Password` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`CustomerID`),
-  UNIQUE KEY `idusers_UNIQUE` (`CustomerID`),
-  UNIQUE KEY `email_UNIQUE` (`Email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  UNIQUE INDEX `idusers_UNIQUE` (`CustomerID` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`Email` ASC))
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `employees` (
-  `EmployeeName` varchar(30) NOT NULL,
-  `EmployeePassword` varchar(45) NOT NULL,
+
+-- -----------------------------------------------------
+-- Table `eshop`.`employees`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eshop`.`employees` (
+  `EmployeeName` VARCHAR(30) NOT NULL,
+  `EmployeePassword` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`EmployeeName`),
-  UNIQUE KEY `EmployeesName_UNIQUE` (`EmployeeName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  UNIQUE INDEX `EmployeesName_UNIQUE` (`EmployeeName` ASC))
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `order` (
-  `OrderID` int(11) NOT NULL AUTO_INCREMENT,
-  `Customer_CustomerID` int(11) NOT NULL,
-  `Products_ProductID` varchar(15) NOT NULL,
-  `Employee_EmployeeName` varchar(30) NOT NULL,
-  `OrderDate` date NOT NULL,
-  `ShippedDate` date NOT NULL,
-  `PaymentMethods` enum('CASH','MASTERCARD') NOT NULL,
-  `Delivery` enum('PICKUP','DELIVERY','DELCOMPANY') NOT NULL,
+
+-- -----------------------------------------------------
+-- Table `eshop`.`order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eshop`.`order` (
+  `OrderID` INT(11) NOT NULL AUTO_INCREMENT,
+  `Customer_CustomerID` INT(11) NOT NULL,
+  `employees_EmployeeName` VARCHAR(30) NOT NULL,
+  `OrderDate` DATE NOT NULL,
+  `ShippedDate` DATE NOT NULL,
+  `PaymentMethods` ENUM('CASH', 'MASTERCARD') NOT NULL,
+  `Delivery` ENUM('PICKUP', 'DELIVERY', 'DELCOMPANY') NOT NULL,
+  `ShipFName` VARCHAR(45) NOT NULL,
+  `ShipLName` VARCHAR(45) NOT NULL,
+  `ShipPhone` INT(11) NOT NULL,
+  `ShipCity` VARCHAR(30) NOT NULL,
+  `ShipAddress` VARCHAR(45) NOT NULL,
+  `ShipEmail` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`OrderID`),
-  UNIQUE KEY `OrderID_UNIQUE` (`OrderID`),
-  UNIQUE KEY `Customer_CustomerID_UNIQUE` (`Customer_CustomerID`),
-  KEY `fk_Order_Customer1_idx` (`Customer_CustomerID`),
-  KEY `fk_Order_Products1_idx` (`Products_ProductID`),
-  CONSTRAINT `fk_Order_Customer1` FOREIGN KEY (`Customer_CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Order_Products1` FOREIGN KEY (`Products_ProductID`) REFERENCES `products` (`ProductID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  UNIQUE INDEX `OrderID_UNIQUE` (`OrderID` ASC),
+  UNIQUE INDEX `Customer_CustomerID_UNIQUE` (`Customer_CustomerID` ASC),
+  INDEX `fk_Order_Customer1_idx` (`Customer_CustomerID` ASC),
+  INDEX `fk_order_employees1_idx` (`employees_EmployeeName` ASC),
+  CONSTRAINT `fk_Order_Customer1`
+  FOREIGN KEY (`Customer_CustomerID`)
+  REFERENCES `eshop`.`customer` (`CustomerID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_employees1`
+  FOREIGN KEY (`employees_EmployeeName`)
+  REFERENCES `eshop`.`employees` (`EmployeeName`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `orderproducts` (
-    `ProductID` VARCHAR(15) NOT NULL,
-    `OrderID` INT(11) NOT NULL,
-    `UnitPrice` FLOAT NOT NULL,
-    `Quantity` INT(11) NOT NULL,
-    `Discount` INT(11) NOT NULL,
-    PRIMARY KEY (`ProductID` , `OrderID`),
-    KEY `fk_products_has_order_order1_idx` (`OrderID`),
-    KEY `fk_products_has_order_products1_idx` (`ProductID`),
-    CONSTRAINT `fk_products_has_order_order1` FOREIGN KEY (`OrderID`)
-        REFERENCES `order` (`OrderID`)
-        ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `fk_products_has_order_products1` FOREIGN KEY (`ProductID`)
-        REFERENCES `products` (`ProductID`)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
-)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-CREATE TABLE `products` (
-  `ProductID` varchar(15) NOT NULL,
-  `ProductName` varchar(60) NOT NULL,
-  `QuantityInStock` int(11) NOT NULL,
-  `Price` int(11) NOT NULL,
-  `Description` varchar(800) NOT NULL,
-  `Brand` varchar(30) NOT NULL,
-  `Category_CategoryID` int(11) NOT NULL,
+-- -----------------------------------------------------
+-- Table `eshop`.`products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eshop`.`products` (
+  `ProductID` VARCHAR(15) NOT NULL,
+  `ProductName` VARCHAR(60) NOT NULL,
+  `QuantityInStock` INT(11) NOT NULL,
+  `Price` INT(11) NOT NULL,
+  `Description` VARCHAR(800) NOT NULL,
+  `Brand` VARCHAR(30) NOT NULL,
+  `Category_CategoryID` INT(11) NOT NULL,
   PRIMARY KEY (`ProductID`),
-  UNIQUE KEY `idproducts_UNIQUE` (`ProductID`),
-  KEY `fk_Products_Category1_idx` (`Category_CategoryID`),
-  CONSTRAINT `fk_Products_Category1` FOREIGN KEY (`Category_CategoryID`) REFERENCES `category` (`CategoryID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  UNIQUE INDEX `idproducts_UNIQUE` (`ProductID` ASC),
+  INDEX `fk_Products_Category1_idx` (`Category_CategoryID` ASC),
+  CONSTRAINT `fk_Products_Category1`
+  FOREIGN KEY (`Category_CategoryID`)
+  REFERENCES `eshop`.`category` (`CategoryID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `eshop`.`orderproducts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eshop`.`orderproducts` (
+  `ProductID` VARCHAR(15) NOT NULL,
+  `OrderID` INT(11) NOT NULL,
+  `UnitPrice` FLOAT NOT NULL,
+  `Quantity` INT(11) NOT NULL,
+  `Discount` INT(11) NOT NULL,
+  PRIMARY KEY (`ProductID`, `OrderID`),
+  INDEX `fk_products_has_order_order1_idx` (`OrderID` ASC),
+  INDEX `fk_products_has_order_products1_idx` (`ProductID` ASC),
+  CONSTRAINT `fk_products_has_order_order1`
+  FOREIGN KEY (`OrderID`)
+  REFERENCES `eshop`.`order` (`OrderID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_products_has_order_products1`
+  FOREIGN KEY (`ProductID`)
+  REFERENCES `eshop`.`products` (`ProductID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- src/main/java/com/kuleshov/WorkWithDb.java
+++ src/main/java/com/kuleshov/WorkWithDb.java
@@ -1,11 +1,11 @@
 package com.kuleshov;
 
-        import com.kuleshov.Entity.Employee;
+import com.kuleshov.Entity.Employee;
 
-        import java.sql.*;
+import java.sql.*;
 
 public class WorkWithDb {
-    public static final String URL = "jdbc:mysql://localhost:3306/eshop_new2";
+    public static final String URL = "jdbc:mysql://localhost:3306/eshop";
     public static final String USERNAME = "root";
     public static final String USERPASSWORD = "admin111";
 
@@ -23,39 +23,35 @@ public class WorkWithDb {
             Statement statement = connection.createStatement();
 
 ////          Create
-//            statement.execute("INSERT INTO customer(cust_id, fed_id, cust_type_cd, address, city, state, postal_code) " +
-//                    "VALUES ('16', '05-555555', 'B', '8585 Presidential Way', 'Quincy', 'MA', '02213');");
-//
+//            statement.execute("INSERT INTO eshop.employees (EmployeeName, EmployeePassword) VALUES ('admin', '12345');");
+
 ////          Create Batch
-//            statement.addBatch("INSERT INTO customer(cust_id, fed_id, cust_type_cd, address, city, state, postal_code) " +
-//                    "VALUES ('14', '05-555555', 'B', '8585 Presidential Way', 'Quincy', 'MA', '02213');");
-//            statement.addBatch("INSERT INTO customer(cust_id, fed_id, cust_type_cd, address, city, state, postal_code) " +
-//                    "VALUES ('15', '05-555555', 'B', '8585 Presidential Way', 'Quincy', 'MA', '02213');");
-//            statement.addBatch("INSERT INTO customer(cust_id, fed_id, cust_type_cd, address, city, state, postal_code) " +
-//                    "VALUES ('16', '05-555555', 'B', '8585 Presidential Way', 'Quincy', 'MA', '02213');");
+//            statement.addBatch("INSERT INTO eshop.employees (EmployeeName, EmployeePassword) VALUES ('Ivan', '54321');");
+//            statement.addBatch("INSERT INTO eshop.employees (EmployeeName, EmployeePassword) VALUES ('Steve', 'qwerty');");
+//            statement.addBatch("INSERT INTO eshop.employees (EmployeeName, EmployeePassword) VALUES ('Alena', 'ytrewq');");
 //            statement.executeBatch();
-//
 //            statement.clearBatch();
 //
 //            boolean status = statement.isClosed();
 //            System.out.println(status);
 
+
+//          Update
+            int resUpdate = statement.executeUpdate("UPDATE eshop.employees SET EmployeePassword='change' WHERE EmployeeName='Steve';");
+            System.out.println(resUpdate);
+
+//          Delete
+            int resDelete = statement.executeUpdate("DELETE FROM eshop.employees WHERE EmployeeName='Steve';\n");
+            System.out.println(resDelete);
+
 //          Read
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM eshop_new2.employees;");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM eshop.employees;");
             while (resultSet.next()) {
                 Employee employee = new Employee();
                 employee.setName(resultSet.getString("EmployeeName"));
                 employee.setPassword(resultSet.getString("EmployeePassword"));
                 System.out.println(employee);
             }
-
-////          Update
-//            int resUpdate = statement.executeUpdate("UPDATE bank.customer SET state='HL' WHERE cust_id='16';");
-//            System.out.println(resUpdate);
-//
-////          Delete
-//            int resDelete = statement.executeUpdate("DELETE FROM bank.customer WHERE cust_id='16';");
-//            System.out.println(resDelete);
 
         } catch (Exception e) {
             e.printStackTrace();
