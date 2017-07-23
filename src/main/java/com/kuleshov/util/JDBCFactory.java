7f66d9a54a49daa1e15fac8cb5bcc75cee7f73bf
Sergey <silver2006@tut.by>
2017-07-23 21:20:12 +0300

    Create class JDBCFactory

---
+++ src/main/java/com/kuleshov/util/JDBCFactory.java
@@ -0,0 +1,36 @@
+package com.kuleshov.util;
+
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCFactory {
+    private static Logger logger = Logger.getLogger(JDBCFactory.class);
+    private static LocalProperties properties = new LocalProperties();
+    private static final String DB_URL = "db.url";
+    private static final String DB_LOGIN = "db.login";
+    private static final String DB_PASSWORD = "db.pass";
+    private static final String SQL_DRIVER = "db.mysql.driver";
+
+    public static Connection getConnection() {
+        try {
+            Connection connection;
+            Class.forName(properties.get(SQL_DRIVER));
+            logger.info("MySQL JDBC Driver Registered!");
+
+            connection = DriverManager
+                    .getConnection(properties.get(DB_URL), properties.get(DB_LOGIN), properties.get(DB_PASSWORD));
+
+            logger.info("connection to database created");
+            return connection;
+        } catch (ClassNotFoundException e) {
+            logger.error("MySQL driver is not loaded");
+            return null;
+        } catch (SQLException e) {
+            logger.error("Connection to database failed.");
+            return null;
+        }
+    }
+}
