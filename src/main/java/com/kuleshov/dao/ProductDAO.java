3e21a9b27f6abe53d4d05662e393eb3d716e05ee
Sergey <silver2006@tut.by>
2017-08-06 11:50:16 +0300

    Create DAO for entity Product

---
+++ src/main/java/com/kuleshov/dao/ProductDAO.java
@@ -0,0 +1,95 @@
+package com.kuleshov.dao;
+
+import com.kuleshov.entity.Product;
+import com.kuleshov.exception.IllegalRequestException;
+import org.apache.log4j.Logger;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductDAO extends AbstractDAO {
+
+    private Logger logger = Logger.getLogger(this.getClass());
+    private String SAVE_QUERY = "INSERT INTO eshop.products (ProductID, ProductName, QuantityInStock, Price, Description, Brand, Category_CategoryID) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?);";
+    private String SELECT_BY_ID_QUERY = "SELECT ProductID, ProductName, QuantityInStock, Price, Description, Brand, Category_CategoryID " +
+            "FROM products WHERE ProductID=?";
+    private String UPDATE_QUERY = "UPDATE eshop.products " +
+            "SET ProductID=?, ProductName=?, QuantityInStock=?, Price=?, Description=?, Brand=?, Category_CategoryID=? WHERE ProductID=?;";
+    private String DELETE_QUERY = "DELETE FROM eshop.products WHERE ProductID=?;";
+
+    public boolean save(Product product) {
+        try (PreparedStatement st = connection.prepareStatement(SAVE_QUERY)) {
+
+            st.setInt(1, product.getProductId());
+            st.setString(2, product.getProductName());
+            st.setInt(3, product.getQuantityInStock());
+            st.setInt(4, product.getPrice());
+            st.setString(5, product.getDescription());
+            st.setString(6, product.getBrand());
+            st.setInt(7, product.getCategoryCategoryId());
+
+            st.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            logger.error("Can't save product with ID: " + product.getProductId());
+            throw new IllegalRequestException("");
+        }
+    }
+
+    public Product findId(int id) {
+        try (PreparedStatement st = connection.prepareStatement(SELECT_BY_ID_QUERY)) {
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                Product product = new Product();
+                product.setProductId(rs.getInt(1));
+                product.setProductName(rs.getString(2));
+                product.setQuantityInStock(rs.getInt(3));
+                product.setPrice(rs.getInt(4));
+                product.setDescription(rs.getString(5));
+                product.setBrand(rs.getString(6));
+                product.setCategoryCategoryId(rs.getInt(7));
+                return product;
+            }
+            return null;
+        } catch (SQLException e) {
+            logger.error("Can't find product with ID: " + id);
+            return null;
+        }
+    }
+
+    public boolean update(Product product) {
+        try (PreparedStatement st = connection.prepareStatement(UPDATE_QUERY)) {
+
+            st.setInt(1, product.getProductId());
+            st.setString(2, product.getProductName());
+            st.setInt(3, product.getQuantityInStock());
+            st.setInt(4, product.getPrice());
+            st.setString(5, product.getDescription());
+            st.setString(6, product.getBrand());
+            st.setInt(7, product.getCategoryCategoryId());
+            st.setInt(8, product.getProductId());
+
+            st.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            logger.error("Can't update product with ID: " + product.getProductId());
+            throw new IllegalRequestException("");
+        }
+    }
+
+    public boolean delete(int id) {
+        try (PreparedStatement st = connection.prepareStatement(DELETE_QUERY)) {
+            st.setInt(1, id);
+
+            st.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            logger.error("Can't delete product with id: " + id);
+            throw new IllegalRequestException("");
+        }
+    }
+}
