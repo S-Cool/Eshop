5e97c9184cce1cb03d47b2cbaac204cc39cda562
Sergey <silver2006@tut.by>
2017-12-03 23:15:18 +0300

    Add registration page

--- src/main/java/com/kuleshov/web/View.java
+++ src/main/java/com/kuleshov/web/View.java
@@ -4,15 +4,28 @@ public enum View {
     LOGIN("login"),
     MAIN("main"),
     ERROR("error"),
-    USER("category");
+    USER("user"),
+    REGISTRATION("registration");
 
     private String name;
+    private String fullName;
+    private static final String BASE_DIRECTORY = "/WEB-INF/view/";
+    private static final String JSP_SUFFIX = ".jsp";
 
     View(String name) {
         this.name = name;
+        this.fullName = BASE_DIRECTORY + name + JSP_SUFFIX;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public static String getBaseDirectory() {
+        return BASE_DIRECTORY;
     }
 }
